3a64a05d1f5e621fac6ae182fef192ec8278090a
mystery Z <mysteryz@Zmysterys-MBP.local>
2019-04-01 15:32:50 +0800

    地铁数据处理

---
+++ metro_data_process/get_metro_location.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+import json
+import io
+import sys
+
+region = {}
+
+def judgeregionid(x, y):
+    for item in region:
+        if int(item['x']) == int(x) and int(item['y']) == int(y):
+            return item['label']
+    return -1
+
+location = []
+with io.open("./metroStation_infor/车站.csv", 'r') as file:
+    next(file)
+    for line in file:
+        da = line.strip('\n').split(',')
+        if da[4] == "" or da[5] =="":
+            da[4] = "0"
+            da[5] = "0"
+        name = da[2].replace(' ', '')
+        loc = str(da[3]) + "号线" + str(da[1]) + '\t' + name + '\t' + str(da[4]) + '\t' + str(da[5])
+        location.append(loc)
+        # print (loc)
+
+#确定站点经纬度
+normal = []
+anomalies = []
+with open("./metroStation_infor/stationLocationGPS.json", "r") as file:
+    data = json.load(file)
+    # print(len(data))
+    for item in location:
+        infor = item.strip('\n').split('\t')
+        if infor[1] in data:
+            itude = data[infor[1]]
+            infor.extend(itude)
+            normal.append(infor)
+        
+        else :
+            anomalies.append(infor)
+    
+
+# 这里从车站.csv删除了严御路这个站,anomalies为空，已经存在metro_station
+# for item in normal:
+#     item = [str(i) for i in item]
+#     str_data = '\t'.join(item)
+#     print(str_data)
+# for item in anomalies:
+#     print(item)
+
+# normal 即为 metro_station
+# 1号线莘庄	Xinzhuang	201	771	121.38033486645138	31.112825888258474
+# 1号线外环路	Waihuanlu	224	738	121.38856113556504	31.123131584366114
+
+
+# print (normal)
+
+for item in normal:
+    E = float(item[4])
+    N = float(item[5])
+
+    x = 745 * (E - 121.31) / 0.53 
+    y = 469 * (31.37 - N) / 0.22
+    
+    item.append(int(x + 0.5))
+    item.append(int(y + 0.5))
+
+# for item in normal:
+#     item = [str(i) for i in item]
+#     str_data = '\t'.join(item)
+#     print(str_data)
+    
+
+
+
+# todo: 根据像素点坐标确定regionid
+with io.open("./metroStation_infor/lable.json", 'r') as file:
+        region = json.load(file)
+
+        new_normal = []
+        for item in normal:
+            x = int(item[6])
+            y = int(item[7])
+            
+            for item2 in region:
+                if int(item2["x"]) == x and int(item2["y"]) == y:
+                    # print(1)
+                    item.append(item2["lable"])
+                    new_normal.append(item)
+                    break
+
+# 此时metro_station 这个文件已经是对应站点的区域划分了，删除掉了没有在选定范围内的站点
+for item in new_normal:
+    item = [str(i) for i in item]
+    str_data = '\t'.join(item)
+    print(str_data)
+  
+
+# for item in normal:
+#     #对应xy位置
+#     #judgeregionid(x, y)
+#     #item.append()
+
+
+
+
+    
+
---
+++ metro_data_process/metro_data.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+import os
+import io
+import sys
+import chardet
+import json
+import time
+import pandas as pd
+
+
+def maketimeslace(timestamp):
+    
+    timeArray = time.localtime(timestamp)
+    otherStyleTime = time.strftime("%H:%M", timeArray)
+    h = int(str(otherStyleTime).split(':')[0])
+    m = int(str(otherStyleTime).split(':')[1])
+    return str(h*2 + m//30)
+
+# 处理地铁站点的区域划分
+metro_id = {}
+with open("./metro_station", 'r') as file:
+    for line in file:
+        infor = line.strip('\n').split('\t')
+        metro_id[infor[0]] = infor[8]
+
+
+
+data = {} #处理csv存的dict，每读取一个csv后都会增加很多，然后处理删除掉一些信息生成新的信息得到res_data
+
+dataroot = "./data/"
+filenamelist = os.listdir(dataroot)
+
+for inputdata in filenamelist:
+    if inputdata.find("csv") < 0 :
+        continue
+    print (inputdata)
+    with io.open(dataroot + inputdata, encoding = "gbk") as file:
+        id = set() #用来判断是否第一次出现该id，在dict中判断时间n，set时间1
+        index = 0
+        for line in file:
+            index += 1
+            if index%1000000 == 0:
+                print(index)
+            line = line.strip('\n').split(',')
+            strtime = line[1] + " " + line[2]
+            timeArray = time.strptime(strtime, "%Y-%m-%d %H:%M:%S")
+            timeStamp = int(time.mktime(timeArray))
+            one_outdata = []
+            one_outdata.append(timeStamp)
+            one_outdata.append(line[3])
+            one_outdata.append(line[5])
+            if line[0] in id:
+                data[line[0]].append(one_outdata)
+            else:
+                data[line[0]] = []
+                data[line[0]].append(one_outdata)
+                id.add(line[0])
+
+    # data = data.encode("utf-8")
+    
+    # with open("./1.json","w") as f:
+    #     json.dump(data,f,ensure_ascii=False)
+    # 此时的data是读取当前文件后的信息，每次读取一个都对data当中id中的出行信息进行一次排序，根据时间戳排序，
+    # 然后根据严重起始 0 终止不等于0来计算一条，然后将这两个信息在id对应list中删除
+    # id start_loc end_loc start_time s_timeslice end_time e_timeslice price
+
+    res_data = [] #处理dict得到的一条条od数据，每个文件单独生成吧
+    for item in data:
+        thedata = sorted(data[item],key=(lambda x:x[0]))
+
+        i = 0
+        while i < len(thedata):
+            if i+1 >= len(thedata):
+                break
+            one = thedata[i]
+            two = thedata[i+1]
+
+            res = []
+            if int(one[0]) < int(two[0]) and one[2] == "0.00" and two[2] != "0.00":
+                
+                res.append(item) #id
+                res.append(one[1]) #起点
+                res.append(two[1]) #终点
+                # 存在起点终点不在选定范围的数据，不加入res_Data但还是要配对删除的
+                if one[1] in metro_id and two[1] in metro_id:
+
+                    res.append(metro_id[one[1]])
+                    res.append(metro_id[two[1]])
+                    res.append(one[0])
+                    res.append(maketimeslace(one[0]))
+                    res.append(two[0])
+                    res.append(maketimeslace(two[0]))
+                    res.append(one[2])
+                    res.append(two[2])
+                    res_data.append(res)
+                
+                # print(res)
+                del thedata[i]
+                del thedata[i]
+            else :
+                i += 1
+
+        data[item] = thedata
+
+    # data每次读取新的文件都会进行更新
+
+    if not os.path.exists("./output"):
+        os.makedirs("./output")
+    outfile = "./output/od_" + inputdata[:-4]
+    with open(outfile, 'w') as file:
+        sum_oneday_data = ""
+        for item in res_data:
+            item = [str(i) for i in item]
+            str_data = '\t'.join(item)
+            # print (str_data)
+            sum_oneday_data = sum_oneday_data + str_data + '\n'
+            
+        file.write(sum_oneday_data.strip('\n'))

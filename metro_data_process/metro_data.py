cd230af5cfe79148c6e96a9c15b3361626e8223b
mystery Z <mysteryz@Zmysterys-MBP.local>
2019-04-01 18:13:18 +0800

    写入文件方法修改

--- metro_data_process/metro_data.py
+++ metro_data_process/metro_data.py
@@ -2,10 +2,8 @@
 import os
 import io
 import sys
-import chardet
 import json
 import time
-import pandas as pd
 
 
 def maketimeslace(timestamp):
@@ -67,11 +65,13 @@ for inputdata in filenamelist:
     res_data = [] #处理dict得到的一条条od数据，每个文件单独生成吧
     for item in data:
         thedata = sorted(data[item],key=(lambda x:x[0]))
-
         i = 0
+
+        res_index = 0
         while i < len(thedata):
             if i+1 >= len(thedata):
                 break
+            
             one = thedata[i]
             two = thedata[i+1]
 
@@ -83,7 +83,6 @@ for inputdata in filenamelist:
                 res.append(two[1]) #终点
                 # 存在起点终点不在选定范围的数据，不加入res_Data但还是要配对删除的
                 if one[1] in metro_id and two[1] in metro_id:
-
                     res.append(metro_id[one[1]])
                     res.append(metro_id[two[1]])
                     res.append(one[0])
@@ -93,7 +92,10 @@ for inputdata in filenamelist:
                     res.append(one[2])
                     res.append(two[2])
                     res_data.append(res)
-                
+
+                    res_index += 1
+                    if res_index%1000 == 0:
+                        print("已经写入：", res_index) 
                 # print(res)
                 del thedata[i]
                 del thedata[i]
@@ -107,12 +109,20 @@ for inputdata in filenamelist:
     if not os.path.exists("./output"):
         os.makedirs("./output")
     outfile = "./output/od_" + inputdata[:-4]
-    with open(outfile, 'w') as file:
-        sum_oneday_data = ""
+
+    with open(outfile, 'a+') as file:
         for item in res_data:
             item = [str(i) for i in item]
             str_data = '\t'.join(item)
-            # print (str_data)
-            sum_oneday_data = sum_oneday_data + str_data + '\n'
+            file.write(str_data)
+
+    print(outfile, "写入完成")
+    # with open(outfile, 'w') as file:
+    #     sum_oneday_data = ""
+    #     for item in res_data:
+    #         item = [str(i) for i in item]
+    #         str_data = '\t'.join(item)
+    #         # print (str_data)
+    #         sum_oneday_data = sum_oneday_data + str_data + '\n'
             
-        file.write(sum_oneday_data.strip('\n'))
+    #     file.write(sum_oneday_data.strip('\n'))
